c54f155b6d59984800c951cb3f7ba01a2292ca5b
Junsang-mun <mun.js@aol.com>
2022-06-29 13:46:34 +0900

    Fix segfault (e8933e8)
    세그폴트 에러 수정; <= 문제, numberArray[i]를 toFind가 아닌 number이랑 비교하고 있었음

--- 220629/10807.c
+++ 220629/10807.c
@@ -4,20 +4,21 @@ int main(void) {
     int number;
     int numberArray[101];
     int toFind;
-    int found;
+    int found = 0;
     int i;
 
     i = 0;
-    scanf("%d", number);
+    scanf("%d", &number);
     while (i < number) {
         scanf("%d", &numberArray[i]);
         i ++;
     }
-    scanf("%d", toFind);
+    scanf("%d", &toFind);
     i = 0;
     while(i < number){
-        if (numberArray[i] = number)
+        if (numberArray[i] == toFind)
             found ++;
         i ++;
     }
+    printf("%d", found);
 }
