da3cae36fbc106b90e25e3eec704dee429adeb21
Junsang-mun <mun.js@aol.com>
2022-06-29 16:29:46 +0900

    Fix; 1712; Logic Improvement;
    1712 로직 개선; 손익분기점을 바로 계산하도록 수식으로 변경

--- 220629/1712.c
+++ 220629/1712.c
@@ -1,21 +1,14 @@
 #include <stdio.h>
 
 int main(void) {
-    int staticCost, variableCost, price;
-    int totalCost, totalSales, quantity;
+    int staticCost, variableCost, price, quantity;
 
     quantity = 0;
-    totalCost = 0;
-    totalSales = 0;
     scanf("%d %d %d", &staticCost, &variableCost, &price);
     if (variableCost >= price) {
         quantity = -1;
     } else {
-        do {
-            totalCost = staticCost + variableCost * quantity;
-            totalSales = price * quantity;
-            quantity ++;
-        } while (totalCost > totalSales);
+        quantity = staticCost / (price - variableCost) + 1;
     } 
     printf("%d", quantity);
 }
