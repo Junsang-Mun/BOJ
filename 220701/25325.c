8b3a7ec8d19cfbe0b1d35849cff8ec50f0c9cb39
Junsang-mun <mun.js@aol.com>
2022-07-01 16:55:18 +0900

    Edit; 25325; 정렬 대신 strcmp
    생각해보니까 머지소트를 사용할 이유가 없었다. strcmp를 사용하면 바로 될 것 같은데...
    strcmp 하나 쓰자고 string.h를 넣기는 좀 그래서 대신 strcmp를 정의해줬다.

--- 220701/25325.c
+++ 220701/25325.c
@@ -4,18 +4,35 @@ typedef struct vote {
     char name[11];
 }vote;
 
+int f_strcmp(const char *s1, const char *s2) {
+    int i = 0;
+
+    while (s1[i] == s2[i] && s1[i] && s2[i]) {
+        i ++;
+    }
+    return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
+
 int main(void) {
     int numberOfStudents;
     int order = 0;
     int called = 0;
 
     scanf("%d", &numberOfStudents);
-    //char vote[numberOfStudents];
     vote STUDENTS[numberOfStudents];
+    vote tmp;
     for (int i = 0; i <= numberOfStudents - 1; i ++) {
         scanf("%s", &STUDENTS[i].name);
     }
-    //mergeSort(vote, 0, numberOfStudents - 1);
+    for (int ii = 0; ii <= numberOfStudents - 1; ii ++) {
+        for (int iii = ii + 1; iii < numberOfStudents; iii ++) {
+            if (f_strcmp(STUDENTS[ii].name, STUDENTS[iii].name) > 0) {
+                tmp = STUDENTS[ii];
+                STUDENTS[ii] = STUDENTS[iii];
+                STUDENTS[iii] = tmp;
+            }
+        }
+    }
     while(order < numberOfStudents) {
         if (STUDENTS[order].name == STUDENTS[order + 1].name) {
             called ++; 
