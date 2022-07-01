ed8727f5d036992429ce3df44a0e5a23d2d8e360
Junsang-mun <mun.js@aol.com>
2022-07-01 15:34:20 +0900

    add; 25325; 기본 로직 작성중
    학생들 모두 받아서 정렬 돌린 다음 숫자 세주면서 출력

---
+++ 220701/25325.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+void merge(char arr[], int left, int mid, int right) {
+    char arrCopy[100], i = left, j = mid+1, k = left;
+    while(i <= mid && j <= right) {
+        if(arr[i] <= arr[j]) arrCopy[k++] = arr[i++];
+        else arrCopy[k++] = arr[j++];
+    }
+    while(i<=mid) arrCopy[k++] = arr[i++];
+    while(j<=right) arrCopy[k++] = arr[j++];
+    for(int a=left; a<=right; a++) arr[a] = arrCopy[a];
+}
+
+void mergeSort(char arr[], int left, int right) {
+    int mid;
+    if(left < right) {
+        mid = (left+right)/2;
+        mergeSort(arr, left, mid);
+        mergeSort(arr, mid+1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+int main(void) {
+    int numberOfStudents;
+
+    scanf("%d", &numberOfStudents);
+    char vote[numberOfStudents];
+    for (int i = 0; i <= numberOfStudents - 1; i ++) {
+        scanf("%s", &vote[i]);
+    }
+    mergeSort(vote, 0, numberOfStudents - 1);
+}
