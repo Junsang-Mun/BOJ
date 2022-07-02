1ef060615db0fa766f18645069053420c5cc2e15
Junsang-mun <mun.js@aol.com>
2022-07-02 22:17:34 +0900

    Fix; 2839; 로직 수정
    - 생각해보니 Edge case가 있었음
    - 11의 경우 5 + 3 + 3 그래서 carry가 3이 나와야 하는데 이전 로직에서는 -1을 출력하는 문제 발생
    - 원인은 weight 5 나머지를 구하는 발상이었다
    - weight가 5나 3으로 나누어떨어지지 않으면 weight를 5씩 빼가면서 weight이 3으로 나뉘는지 확인
    - 나뉜다면 carry 값을 정상적으로 가지게 되고, weight가 0 이하가 된다면 carry에 -1을 던지고 break한다.

--- 220702/2839.c
+++ 220702/2839.c
@@ -3,17 +3,28 @@
 int main(void) {
     int weight;
     int carry;
+    int i;
 
+    i = 0;
     scanf("%d", &weight);
     if (weight % 5 == 0) {
         carry = weight / 5;
-    } else if ((weight / 5) % 3 == 0) {
-        carry = weight / 5;
-        carry += (weight / 5) % 3;
     } else if (weight % 3 == 0) {
         carry = weight / 3;
     } else {
-        carry = -1;
+        while (1) {
+            weight -= 5;
+            i ++;
+            if ((weight != 0) && (weight % 3 == 0)) {
+                carry = i;
+                carry += weight / 3;
+                break ;
+            }
+            if (weight <= 0) {
+                carry = -1;
+                break ;
+            }
+        }
     }
     printf("%d", carry);
     return 0; 
