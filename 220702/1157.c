f2afe59a8700e02940e2fe10c6c03d8ed4294e53
Junsang-mun <mun.js@aol.com>
2022-07-02 13:50:39 +0900

    Add; 1157; 기본 로직 작성 중
    - arr에 입력을 받아 arr[i] == NUL까지 돌린다.
    - arr[i]를 대소문자 무시하고 알파벳 순서로 1~26을 Assign한다.
    - Displayed의 arr[i] - 1번째가 나올때마다 displayed[arr[i]-1]번째를 더한다.
    - displayed의 최댓값을 찾아내서 출력한당.

---
+++ 220702/1157.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+    char arr[1000001];
+    int displayed[26] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+    int i = 0;
+
+    while (scanf("%1s", &arr[i]) != EOF) {
+        if (arr[i] > 96)
+            arr[i] -= 32;
+        arr[i] -= 64;
+        i ++;
+    }
+    i = 0;
+    while (arr[i] != 0) {
+        displayed[(int)arr[i] - 1] ++;
+        i ++;
+    }
+}
