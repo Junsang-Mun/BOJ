6a217f6f7801b0ba844148690d2b46d0794e8051
Junsang-mun <mun.js@aol.com>
2022-07-02 22:01:24 +0900

    Edit; 2839; 로직 수정
    - 생각해보니까 5로 먼저 나눌 것이 아니라 if문 통해 4가지 option으로 분기하는 것이 나은 방법으로 판단함
    - 5로 나뉘는 경우, 5로 나누고 나머지가 3으로 나뉘는 경우, 3으로 나뉘는 경우, else 4가지 분기 뽑아줌

--- 220702/2839.c
+++ 220702/2839.c
@@ -5,9 +5,13 @@ int main(void) {
     int carry;
 
     scanf("%d", &weight);
-    carry = weight / 5;
-    if ((weight / 5) % 3 == 0) {
+    if (weight % 5 == 0) {
+        carry = weight / 5;
+    } else if ((weight / 5) % 3 == 0) {
+        carry = weight / 5;
         carry += (weight / 5) % 3;
+    } else if (weight % 3 == 0) {
+        carry = weight / 3;
     } else {
         carry = -1;
     }
