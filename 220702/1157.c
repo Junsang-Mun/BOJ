6807a1fc87b58df65bc266ff68d5b75a5fdebd59
Junsang-mun <mun.js@aol.com>
2022-07-02 14:08:02 +0900

    Add; 1157; 출력 함수 작성
    - displayed 최댓값을 찾아서 출력하는 로직을 작성했습니다.

--- 220702/1157.c
+++ 220702/1157.c
@@ -4,6 +4,8 @@ int main(void) {
     char arr[1000001];
     int displayed[26] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
     int i = 0;
+    int large = 0;
+    int match = 0;
 
     while (scanf("%1s", &arr[i]) != EOF) {
         if (arr[i] > 96)
@@ -16,4 +18,15 @@ int main(void) {
         displayed[(int)arr[i] - 1] ++;
         i ++;
     }
+    i = 0;
+    while (i < 26) {
+        if (displayed[i] > large) {
+            large = displayed[i];
+            match = i + 65;
+        } else if (displayed[i] == large) {
+            match = '?';
+        }
+        i ++;
+    }
+    printf("%c", (char)match);
 }
