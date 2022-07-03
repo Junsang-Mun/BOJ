5e7a3aaf1d1af46013f5f4bbe366784a2390a0ed
Junsang-mun <mun.js@aol.com>
2022-07-03 14:37:46 +0900

    Fix; 1463; 로직 개편
    - 동적 프로그래밍으로 로직 개편

--- 220703/1463.c
+++ 220703/1463.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
 
-int main(void) {
-    int value, try;
-
-    try = 0;
-    scanf("%d", &value);
-    while (value != 1) {
-        if (value % 3 == 0) {
-            value /= 3;
-        } else if (value % 2 == 0) {
-            value /= 2;
-        } else {
-            value -= 1;
-        }
-        try ++;
+int min(int a, int b) {
+    if (a < b) {
+        return a;
+    } else if (a > b) {
+        return b;
     }
-    printf("%d", try);
+    return a;
+}
+
+int main(void) {
+    int value;
+    int a[1000001] = {0, };
+
+    scanf("%d", &value);
+    a[1] = 0;
+    for (int i = 2; i <= value; i ++) {
+        a[i] = a[i - 1] + 1;
+        if (i % 2 == 0) {
+            a[i] = min(a[i], a[i / 2] + 1);
+        }
+        if (i % 3 == 0) {
+            a[i] = min(a[i], a[i / 3] + 1);
+        }
+    }
+    printf("%d", a[value]);
 }
