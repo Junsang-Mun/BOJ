7fb7ee08d21a1dd6693dd797c039de871891011c
Junsang-mun <mun.js@aol.com>
2022-07-06 16:09:08 +0900

    Fix; 9657; dp 인덱스값 참조 오류 수정
    - 인덱스 1을 참조해야 할 때 인덱스 0을 참조하고 있었다
    - i와 n값을 조절할까 고민해보았으나 코드가 복잡해지고 이해하기가 어려워서(즉 fun하고 cool하고 sexy하지 않아서)
    - dp[0]에 더미 데이터를 집어넣는 것으로 해결

--- 220706/9657.c
+++ 220706/9657.c
@@ -3,10 +3,11 @@
 int main(void) {
     int n;
     int i = 6;
-    int dp[1001] = {1, 0, 1, 1, 1};
+    int dp[1001] = {0, 1, 0, 1, 1, 1};
     
     scanf("%d", &n);
     while (i <= n) {
+        dp[i] = 0;
         if (dp[i - 1] == 0 || dp[i - 3] == 0 || dp[i - 4] == 0)
             dp[i] = 1;
         i ++;
