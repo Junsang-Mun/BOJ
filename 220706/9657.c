e8da3ea465a51cbee6d50c7b112912fb1204607d
Junsang-mun <mun.js@aol.com>
2022-07-06 16:00:26 +0900

    Fix; 9657; dp로 로직 변경
    - 역시 .. dp는 어려워잉
    - 로직은 맞는 것 같은데 틀렷다고 나옴... 뭐가 문제지??

--- 220706/9657.c
+++ 220706/9657.c
@@ -2,12 +2,19 @@
 
 int main(void) {
     int n;
-
+    int i = 6;
+    int dp[1001] = {1, 0, 1, 1, 1};
+    
     scanf("%d", &n);
-    if (n % 5 == 2) {
-        printf("CY");
-    } else {
-        printf("SK");
+    while (i <= n) {
+        if (dp[i - 1] == 0 || dp[i - 3] == 0 || dp[i - 4] == 0)
+            dp[i] = 1;
+        i ++;
     }
+
+    if (dp[n] == 1)
+        printf("SK");
+    else
+        printf("CY");
     return 0;
 }
