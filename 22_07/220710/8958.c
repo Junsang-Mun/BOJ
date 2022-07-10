e6645ae8a32902ccef067775d519d68a4f087403
Junsang-mun <mun.js@aol.com>
2022-07-10 13:34:24 +0900

    Fix; 8958; wrong answer 해결
    - 변수 초기화 문제였음
    - 변수를 항상 다시 선언해주니 별 문제 없었다.

--- 22_07/220710/8958.c
+++ 22_07/220710/8958.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 int main(void) {
-    int caseNum, i = 0, ii = 0, output = 0;
-    int arr[80] = {0, };
+    int caseNum, i = 0;
+    char arr[80];
 
     scanf("%d", &caseNum);
     while (caseNum > i) {
-        scanf("%c", arr);
-        while (arr[ii]) {
-            if (arr[ii] == 'O')
-                output ++;
+        int ii = 0, output = 0, strike = 1;
+        scanf("%s", arr);
+        while (arr[ii] != 0) {
+            if (arr[ii] == 'O') {
+                output += strike ++;
+            } else {
+                strike = 1;
+            }
             ii ++;
         }
         printf("%d\n", output);
-        output = 0;
         i ++;
     }
 }
