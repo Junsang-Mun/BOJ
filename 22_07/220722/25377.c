4ab6a465046594c1fc26d3796f8ef470907cd4a2
Junsang-mun <mun.js@aol.com>
2022-07-22 14:40:16 +0900

    fix; 25377; idkwhatthehellisgoingon;
    - 52점 맞음
    - ?????????

--- 22_07/220722/25377.c
+++ 22_07/220722/25377.c
@@ -1,21 +1,26 @@
 #include <stdio.h>
 
+int isPegion(int v) {
+    if (v == 9999)
+        return 1;
+    else
+        return 0;
+}
+
 int main(void) {
-    int tcn, bread, run, i = 0, min = -1;
+    int tcn, bread, run, i = 0, min = 9999;
 
     scanf("%d", &tcn);
     while (i < tcn) {
-        scanf("%d %d", &bread, &run);
-        if (bread == 0 && run == 0) {
-            printf("0");
-            return 0;
-        }
-        if (bread <= run) {
-            if (min > bread - run)
-                min = bread - run;
+        scanf("%d %d", &run, &bread);
+        if (run <= bread) {
+            if (min > run)
+                min = bread;
         }
         i ++;
     }
+    if (isPegion(min))
+        min = -1;
     printf("%d", min);
     return 0;
 }
