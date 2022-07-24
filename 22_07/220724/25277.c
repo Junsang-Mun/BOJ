aa03e1c7f3474acbf35bc6c6a9fd04784528b25c
Junsang-mun <mun.js@aol.com>
2022-07-24 10:42:00 +0900

    Edit; 25277; 문제가 생겼어!
    자꾸 값이 튑니다

--- 22_07/220724/25277.c
+++ 22_07/220724/25277.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+#include <string.h>
+
+void a_strlwr(char *s1) {
+  while (*s1 != 0) {
+    if (*s1 >= 65 && *s1 <= 90)
+        *s1 += 32;
+    s1++;
+  }
+}
+
+int fuck(char ch) {
+    //a_strlwr(&ch);
+    if (strcmp(&ch, "he") == 0 || strcmp(&ch, "she") == 0 || strcmp(&ch, "him") == 0 || strcmp(&ch, "her") == 0) {
+        return 1;
+    }
+    return 0;
+}
 
 int main (void) {
     int num, i = 0, tlqkf = 0;
@@ -7,7 +24,7 @@ int main (void) {
     while (i < num) {
         char ch;
         scanf("%s", &ch);
-        if (ch == "he" || ch == "she" || ch == "him" || ch == "her")
+        if (strcmp(&ch, "he") == 0 || strcmp(&ch, "she") == 0 || strcmp(&ch, "him") == 0 || strcmp(&ch, "her") == 0)
             tlqkf ++;
         i ++;
     }
