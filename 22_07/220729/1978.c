f37d54ec6a1ba88c94d18f1c623fc5c4042f2520
Junsang-mun <mun.js@aol.com>
2022-07-29 12:43:23 +0900

    Add; 1978; ACPT
    - n 1 예외처리 좀 고생햇지만
    - 우하하 맞았다

---
+++ 22_07/220729/1978.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+    int n, nn, ans = 0;
+
+    scanf("%d", &nn);
+    for (int a = 0; a < nn; a ++) {
+        int i = 2;
+
+        scanf("%d", &n);
+        while (i * i <= n) {
+            if (n % i == 0) {
+                ans -= 1;
+                break;
+            }
+            i += 1;
+        }
+        if (n == 1) {
+            ans --;
+        }
+        ans += 1;
+    }
+    printf("%d", ans);
+}
