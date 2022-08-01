1f15088a91c53e6a5cfc1dab5decf42cc82f2708
Junsang-Mun <mun.js@aol.com>
2022-08-01 19:56:16 +0900

    Add; 1924; SEGFALT;
    - 9월부터 SEGFALT 뜸
    왜지?

---
+++ 22_08/01/1924.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+	int month, day, md = 0;
+	int monthArray[13] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+	char *dayArray[8] = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+
+	scanf("%d %d", &month, &day);
+	for (int i = 0; i < month - 1; i ++) {
+		md += monthArray[i];
+	}
+	day += md;
+	printf("%s", dayArray[(day % 7) - 1]);
+}
