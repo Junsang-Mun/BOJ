d518979e85d4e8fad7df2c3a1d9614e3e3b8ca64
Junsang-Mun <mun.js@aol.com>
2022-08-04 15:07:56 +0900

    Fix; 10808; ACPT
    - 젠장... 어레이가 0-25 출력이 되어야 하는데 0-26 출력이 되고 있었다
    - 수정 후 ACPT

--- 22_08/04/10808.c
+++ 22_08/04/10808.c
@@ -7,7 +7,7 @@ int main(void) {
 	for (int i = 0; i < 101; i ++) {
 		b[(int)a[i] - 97] += 1;
 	}
-	for (int i = 0; i < 26; i ++)
+	for (int i = 0; i < 25; i ++)
 		printf("%d ", b[i]);
-	printf("%d", b[26]);
+	printf("%d", b[25]);
 }
