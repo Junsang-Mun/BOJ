0e72fbe775017ec98d4eb82cf58e7b7ba87e30dd
Mun Jun Sang <junsang@Muns-MacBook-Air.local>
2022-08-04 14:44:35 +0900

    Add; 10808; FAIL

--- 22_08/04/10808.c
+++ 22_08/04/10808.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
 int main(void) {
-	char a;
+	char a[101];
 	int b[27] = {0, };
-	while (scanf("%s", &a) != EOF) {
-		b[(int)a - 97] += 1;
+	scanf("%s", a);
+	for (int i = 0; i < 101; i ++) {
+		b[(int)a[i] - 97] += 1;
 	}
 	for (int i = 0; i < 26; i ++)
 		printf("%d ", b[i]);
