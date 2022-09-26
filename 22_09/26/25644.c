bcba3d9d0a56a58f8b1d9e5e71d5f7bf30eb43e4
Junsang Mun <mun.js@aol.com>
2022-09-26 17:48:47 +0900

    분명 맞는 것 같은데 fail?

--- 22_09/26/25644.c
+++ 22_09/26/25644.c
@@ -5,11 +5,15 @@ int main(void) {
 	int max = 0, min = __INT_MAX__;
 	scanf ("%d", &curr);
 	while (scanf("%d", &next) != -1) {
-		if (curr < next && max < next)
+		if (curr < next)
 			max = next;
-		if (curr > next && min > next)
+		if (curr > next)
 			min = next;
 		curr = next;
 	}
-	printf("min: %d | max: %d | max-min: %d", min, max, max-min);
+	if (max == 0)
+		printf("0");
+	else
+		printf("%d", max-min);
+	return 0;
 }
