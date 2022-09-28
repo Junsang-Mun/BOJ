267f0900d8211d4cd27428098bb93f978fbc544d
Junsang Mun <mun.js@aol.com>
2022-09-28 22:00:09 +0900

    add 1264 fail

---
+++ 22_09/28/1264.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/** char형 변수를 입력받아 변수가 aeiou인지 검사함 */
+int match(char c) {
+	if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'
+		|| c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+		return (1);
+	return (0);
+}
+
+int main(void) {
+	char c;
+	int count = 0;
+
+	while (1) {
+		scanf("%c", &c);
+		if (c == '#')
+			break;
+		else if (c == '\n') {
+			printf("%d", count);
+			count = 0;
+		}
+		else if (match(c))
+			count ++;
+	}
+}
