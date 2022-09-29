b154db6e32cedf734720e447354ad05fdcf97ac4
Junsang Mun <mun.js@aol.com>
2022-09-29 17:48:24 +0900

    1008 함수 배치 수정

--- 22_09/29/1008.c
+++ 22_09/29/1008.c
@@ -1,30 +1,6 @@
 #include <unistd.h>
 
-void __PUTCHAR(char c) {
-	write(1, &c, 1);
-}
-
-void __PDIGIT(int n) {
-	if (n < 10) {
-		__PUTCHAR(n + '0');
-	} else {
-		__PDIGIT(n / 10);
-		__PDIGIT(n % 10);
-	}
-}
-
-void lfprint(double n) {
-	__PDIGIT((int)n);
-	__PUTCHAR('.');
-	int TO_REMOVE = (int)n;
-	n = n - TO_REMOVE;
-	for (int i = 0; i < 9; i ++) {
-		n *= 10;
-		TO_REMOVE = (int)n;
-		__PUTCHAR((int)n + '0');
-		n = n - TO_REMOVE;
-	}
-}
+void lfprint(double n);
 
 int main(void) {
 	int a = 0, b = 0, pn = 1;
@@ -55,4 +31,30 @@ int main(void) {
 	result = (double)a / (double)b;
 	lfprint(result);
 	return 0;
+}
+
+void __PUTCHAR(char c) {
+	write(1, &c, 1);
+}
+
+void __PDIGIT(int n) {
+	if (n < 10) {
+		__PUTCHAR(n + '0');
+	} else {
+		__PDIGIT(n / 10);
+		__PDIGIT(n % 10);
+	}
+}
+
+void lfprint(double n) {
+	__PDIGIT((int)n);
+	__PUTCHAR('.');
+	int TO_REMOVE = (int)n;
+	n = n - TO_REMOVE;
+	for (int i = 0; i < 9; i ++) {
+		n *= 10;
+		TO_REMOVE = (int)n;
+		__PUTCHAR((int)n + '0');
+		n = n - TO_REMOVE;
+	}
 }
