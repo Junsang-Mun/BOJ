8ba3722fedeb9cf6ea3bbdb04d4ecb18ebd03d8d
Junsang Mun <mun.js@aol.com>
2022-09-29 23:00:52 +0900

    fix 9251 segfault pass

--- 22_09/29/9251.c
+++ 22_09/29/9251.c
@@ -7,23 +7,24 @@ int choice(int a, int b) {
 }
 
 int main(void) {
-	char *a, *b;
+	char a[1001] = {0, }, b[1001] = {0, };
 	int a_len, b_len;
 
 	scanf("%s", a);
 	scanf("%s", b);
 	a_len = strlen(a);
 	b_len = strlen(b);
-	int dp[a_len][b_len];
+	int dp[a_len + 1][b_len + 1];
 	for (int i = 0; i <= b_len; i ++)
 		dp[0][i] = 0;
 	for (int i = 0; i <= a_len; i ++)
 		dp[i][0] = 0;
 	for (int i = 1; i <= a_len; i ++) {
-		for (int ii = 1; ii <= b_len; i ++) {
-			dp[i][ii] = dp[i - 1][ii - 1] + 1;
+		for (int ii = 1; ii <= b_len; ii ++) {
 			if (a[i - 1] != b[ii - 1]) {
 				dp[i][ii] = choice(dp[i - 1][ii], dp[i][ii - 1]);
+			} else {
+				dp[i][ii] = dp[i - 1][ii - 1] + 1;
 			}
 		}
 	}
