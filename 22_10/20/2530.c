a12cd0844455dc9c1455301e4a10fb40450c7381
Junsang Mun <mun.js@aol.com>
2022-10-20 20:09:27 +0900

    2530 working on

---
+++ 22_10/20/2530.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main(void) {
+	int hr, min, sec;
+	int time_hr = 0, time_min = 0, time_sec;
+
+	scanf("%d %d %d", &hr, &min, &sec);
+	scanf("%d", &time_sec);
+	while (time_sec >= 60) {
+		time_sec -= 60;
+		time_min ++;
+	}
+	while (time_min >= 60) {
+		time_min -= 60;
+		time_hr ++;
+	}
+	printf("time_: %d %d %d\n", time_hr, time_min, time_sec);
+	hr += time_hr;
+	min += time_min;
+	sec += time_sec;
+	printf("-: %d %d %d\n", hr, min, sec);
+	while (sec >= 60) {
+		sec -= 60;
+		min ++;
+	}
+	while (min >= 60) {
+		min -= 60;
+		hr ++;
+	}
+	printf("total: %d %d %d", hr, min, sec);
+}
