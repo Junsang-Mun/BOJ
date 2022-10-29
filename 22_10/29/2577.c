af07185517749a74603a7cceed458b9c9c4c4a5d
Junsang Mun <mun.js@aol.com>
2022-10-29 19:19:00 +0900

    2577 pass

--- 22_10/29/2577.c
+++ 22_10/29/2577.c
@@ -1,43 +1,21 @@
 #include <stdio.h>
 
-void itoa(char *ch, int nb) {
-	int i = 10;
-	char buf;
-
+void trustmeiamadeveloper(int *ch, int nb) {
 	while (nb) {
-		ch[i] = (nb % 10) + '0';
+		ch[nb % 10] += 1;
 		nb /= 10;
-		i --;
-	}
-	while (i) {
-		ch[i] = 0;
-		i --;
-	}
-	for (int ii = 0; ii < 5; ii ++) {
-		buf = ch[ii];
-		ch[ii] = ch[9 - ii];
-		ch[9 - ii] = buf;
 	}
 }
 
 int main(void) {
 	int buf, abc = 1, i = 0;
 	int result[10] = {0, };
-	char c[11];
 
 	while (scanf("%d", &buf) != EOF) {
 		abc *= buf;
 	}
-	//DEBUG
-	printf("DEBUG: Value of abc: %d\n", abc);
-	itoa(c, abc);
-	//DEBUG
-	printf("DEBUG: Value of c: %s\n", c);
-	while (c[i]) {
-		result[(int)c[i] - '0'] += 1;
-		i ++;
-	}
+	trustmeiamadeveloper(result, abc);
 	for (int q = 0; q < 10; q ++) {
-		printf("%d: %d\n", q, result[q]);
+		printf("%d\n", result[q]);
 	}
 }
