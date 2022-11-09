4f8f2cc76511b8c80aa6bde2ca4d78f03a5a3c66
Junsang Mun <mun.js@aol.com>
2022-11-09 13:25:23 +0900

    InsufficientSpace??

--- 22_11/09/11365.c
+++ 22_11/09/11365.c
@@ -1,26 +1,62 @@
-#include <stdio.h>
 #include <unistd.h>
 
-int main(void) {
-	char set[501];
+int scan(char *ch) {
 	char buf = 0;
+	int i = 0;
+	while (buf != '\n') {
+		read(1, &buf, 1);
+		ch[i] = buf;
+		i ++;
+	}
+	return i;
+}
 
-	while (1) {
-		int i = 0;
-		while (buf != '\n') {
-			read(1, &buf, 1);
-			set[i] = buf;
+int validate(char *ch) {
+	if (ch[0] == 'E' && ch[1] == 'N' && ch[2] == 'D') {
+		return 1;
+	}
+	return 0;
+}
+
+/**
+ * @brief a print function
+ * 
+ * @param ch a set of characters or a single character pointer
+ * @param flag 1: single char, 2: string, 3: reverse string
+ * @param idx starting index when uses with flag 3; else MUST set to -1
+ */
+void print(char *ch, int flag, int idx) {
+	switch (flag) {
+	int i = 0;
+	case 1:
+		write(1, &ch, 1);
+		break ;
+	case 2:
+		while (ch[i]) {
+			write(1, &ch[i], 1);
 			i ++;
 		}
-		i --;
-		if (set[0] == 'E' && set[1] == 'N' && set[2] == 'D') {
+		break ;
+	case 3:
+		while (idx >= 0) {
+			write(1, &ch[idx], 1);
+			idx --;
+		}
+		break ;
+	case 4:
+		write(1, "\n", 1);
+		break ;
+	}
+}
+
+int main(void) {
+	while (1) {
+		char set[501];
+		int idx = scan(set);
+		if (validate(set)) {
 			return 0;
 		}
-		while (i >= 0) {
-			write(1, &set[i], 1);
-			i --;
-		}
-		printf("\n");
+		print(set, 3, idx);
+		print(0, 4, -1);
 	}
-		//DEBUG: printf("%s", set);
 }
