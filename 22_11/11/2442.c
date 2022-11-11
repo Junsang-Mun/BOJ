aba54f9b5c123673b81aa51d1418c27dd46fa62b
Junsang Mun <mun.js@aol.com>
2022-11-11 13:31:00 +0900

    2442 acpt

---
+++ 22_11/11/2442.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(void) {
+	int n;
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i ++) {
+		int ttal = i + n;
+		int star = i * 2 + 1;
+		int spce = ttal - star;
+		for (int j = 0; j < spce; j ++) {
+			printf(" ");
+		}
+		for (int j = 0; j < star; j ++) {
+			printf("*");
+		}
+		printf("\n");
+	}
+}
